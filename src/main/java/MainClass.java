a5e60148fa4ace5859c5218c636e5ff3df9a554b
Vladislav Zaytsev <vlad_z88@mail.ru>
2016-02-25 13:40:04 +0300

    inputStream was added

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,8 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by QH on 25.02.2016.
  */
 public class MainClass {
     public static void main(String[] args) throws Exception{
-        double [] solves = SolveSquareEq.solve(5,5,2);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double a = Double.parseDouble(reader.readLine());
+        double b = Double.parseDouble(reader.readLine());
+        double c = Double.parseDouble(reader.readLine());
+
+
+        double [] solves = SolveSquareEq.solve(a, b, c);
+        for (double solution : solves)
+        {
+            System.out.println(solution);
+        }
     }
 }
